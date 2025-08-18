3ca3a39eae018ce96f8acbef045c36f298e4c396
cyrilbulanov <cyril.bulanov@yandex.ru>
2025-08-18 17:21:42 +0530

    first workable version

--- lab03/messages_consumer.py
+++ lab03/messages_consumer.py
@@ -5,10 +5,10 @@ import json
 from confluent_kafka import SerializingProducer
 from lab03_lib import User, deserialize_from_utf8, deserialize_from_utf8_json
 
-#app = faust.App("lab03", broker="kafka://127.0.0.1:9094", store="rocksdb://", topic_partition=8)
-app = faust.App("lab03", broker="kafka://127.0.0.1:9094", store="memory://", topic_partition=8)
-tbl_blocked_users = app.Table('tbl_blocked_users', default=str)
-tbl_censored_words = app.Table('tbl_censored_words', default=str)
+app = faust.App("lab03", broker="kafka://127.0.0.1:9094", store="rocksdb://", topic_partition=8)
+#app = faust.App("lab03", broker="kafka://127.0.0.1:9094", store="memory://", topic_partition=8)
+tbl_blocked_users = app.GlobalTable('tbl_blocked_users', default=str)
+tbl_censored_words = app.GlobalTable('tbl_censored_words', default=str)
 tpc_blocked_users = app.topic('blocked_users')
 tpc_censored_words = app.topic('censored_words')
 tpc_messages = app.topic('messages')
@@ -23,19 +23,12 @@ async def process_blocked_users(stream):
 @app.agent(tpc_censored_words)
 async def process_censored_words(stream):
 	async for words in stream:
-		if await to_be_reset(words):
-			await reset_tbl(tbl_censored_words)
-			return
-		tbl_censored_words['censored_words'] = words
+		if words == '[]':
+			del tbl_censored_words['censored_words']
+		else:
+			tbl_censored_words['censored_words'] = words
 
-async def to_be_reset(msg):
-	return msg == '[]'
-
-async def reset_tbl(tbl):
-	async for key in tbl.keys():
-		del tbl[key]
-
-async def process_censored_words(data):
+def process_censored_words(data):
 	result = data
 	for censored_word in tbl_censored_words['censored_words']:
 		result = result.replace(censored_word, '!!!CENSORED!!!')
@@ -43,16 +36,13 @@ async def process_censored_words(data):
 	
 @app.agent(tpc_messages)
 async def process_messages(stream):
-	i = 0
 	async for msg_data in stream:
-		msg_data['payload'] = await process_censored_words(msg_data['payload'])
+		msg_data['payload'] = process_censored_words(msg_data['payload'])
 		sender, receiver = msg_data['sender'], msg_data['receiver']
 		if sender in tbl_blocked_users[receiver]:
 			print(f'{sender} found in the black list of {receiver}')
 		else:
-			#print(f'receiver is not found in the black list...')
-			await tpc_filtered_messages.send(key=f'key-{i}', value=msg_data)
-		i += 1
+			await tpc_filtered_messages.send(key=f'key-{sender}', value=msg_data)
 
 if __name__ == "__main__":
 	app.main()
--- lab03/messages_producer.py
+++ lab03/messages_producer.py
@@ -18,25 +18,33 @@ conf_text = {	'bootstrap.servers': '127.0.0.1:9094',
 		'key.serializer': serialize_to_utf8,
 		'value.serializer': serialize_to_utf8,
 		'acks': 'all',
+		'enable.idempotence': True,
 }
 
 conf_json = {	'bootstrap.servers': '127.0.0.1:9094',
 		'key.serializer': serialize_to_utf8,
 		'value.serializer': serialize_to_json_utf8,
 		'acks': 'all',
+		'enable.idempotence': True,
 }
+
+def handle_delivery(err, msg):
+	if err is not None:
+		print(f'Message delivery failed: {err}')
+	else:
+		print(f'Message delivered to {msg.topic()} topic [{msg.partition()}] @ offset {msg.offset()}')
 
 def produce_black_list():
 	users = load_users('./users.json')
 	producer = SerializingProducer(conf_json)
 	for user in users:
-		producer.produce(topic='blocked_users', key=user.name, value=user)
+		producer.produce(topic='blocked_users', key=user.name, value=user, on_delivery=handle_delivery)
 	producer.flush()
 
 def produce_censored_words(file):
 	censored_words = [word for word in load_json(file)]
 	producer = SerializingProducer(conf_text)
-	producer.produce(topic='censored_words', key='word_list', value=json.dumps(censored_words))
+	producer.produce(topic='censored_words', key='word_list', value=json.dumps(censored_words), on_delivery=handle_delivery)
 	producer.flush()
 
 def produce_messages(message_count):
@@ -46,10 +54,11 @@ def produce_messages(message_count):
 		sender, receiver, payload = random.choice(users).name, random.choice(users).name, generate_random_sentence()
 		msg_header = f'key={sender}'
 		msg_val = {'sender': sender, 'receiver': receiver, 'payload': payload}
-		producer.produce(topic='messages', key=msg_header, value=json.dumps(msg_val))
+		producer.produce(topic='messages', key=msg_header, value=json.dumps(msg_val), on_delivery=handle_delivery)
 		if (i + 1) % 50_000 == 0:
 			producer.flush()
 		time.sleep(random.randint(1, 5))
+	producer.flush()
 
 def main():
 	args = parser.parse_args()
